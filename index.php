48f2d43dc6b9d8bcc593c7bf02af036bb3dc298f
deependrabhadoriya4500@gmail.com <deependrabhadoriya4500@gmail.com>
2019-01-30 14:49:50 +0530

    all file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+    <h4>Hii,<BR>PHP</h4>
+    <?PHP
+   $a=3;
+   $b="ritik";
+   $c="bansal";
+   /*$d=echo $a;*/
+   /*$p= echo "string";*/
+   echo $a;
+    ?>
+   
+
+</body>
+</html>
