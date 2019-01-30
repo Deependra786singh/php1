3009162f1845d583015e57a555b3a00a9c5ee7f3
deependra786singh <deependrabhadoriya4500@gmail.com>
2019-01-30 15:37:19 +0530

    all file

---
+++ firstgit.php
@@ -0,0 +1,23 @@
+<?php
+$a = 10;
+$b= 20;
+if($a>$b)
+{
+	echo "a greater then b";
+}
+elseif($a<$b)
+{
+	echo "a smaller then b";
+}
+// else($a=$b)
+// {
+// 	echo "a equal then b";
+// }
+
+
+
+
+
+
+
+?>
